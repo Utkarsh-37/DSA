6a7c2b0eeb1b0e5c23e946452f640964933a627b
Utkarsh-37 <up.om.mgs@gmail.com>
2025-07-03 12:20:14 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (41.75%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -11,18 +11,48 @@
  */
 class Solution {
 public:
-    void post(TreeNode *root, vector<int> &soln)
+    // 2 Stack iterative Approach
+    vector<int> postorderTraversal(TreeNode* root)
     {
-        if(root == NULL) return;
-
-        post(root->left, soln);
-        post(root->right, soln);
-        soln.push_back(root->val);
-    }
-
-    vector<int> postorderTraversal(TreeNode* root) {
         vector<int> soln;
-        post(root, soln);
+        if(root == NULL) return soln;
+        stack<TreeNode*> st1;
+        stack<TreeNode*> st2;
+        st1.push(root);
+        while(st1.empty() == false)
+        {
+            TreeNode* node = st1.top();
+            st1.pop();
+            st2.push(node);
+            if(node->left != NULL)
+                st1.push(node->left);
+            if(node->right != NULL)
+                st1.push(node->right);
+        }
+        while(st2.empty() == false)
+        {
+            TreeNode* node = st2.top();
+            soln.push_back(node->val);
+            st2.pop();
+        }
         return soln;
     }
+
+
+
+    // Recursive Approach
+    // void post(TreeNode *root, vector<int> &soln)
+    // {
+    //     if(root == NULL) return;
+
+    //     post(root->left, soln);
+    //     post(root->right, soln);
+    //     soln.push_back(root->val);
+    // }
+
+    // vector<int> postorderTraversal(TreeNode* root) {
+    //     vector<int> soln;
+    //     post(root, soln);
+    //     return soln;
+    // }
 };
