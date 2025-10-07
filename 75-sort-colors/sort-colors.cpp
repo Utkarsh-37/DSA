3cee716ab446545754bd6cc201d1485b74d5aad2
Utkarsh-37 <up.om.mgs@gmail.com>
2025-10-07 10:27:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (48.97%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -2,6 +2,29 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
+        //O(n) approach
+        int low = 0, mid = 0, high = n-1;
+        while(mid <= high)
+        {
+            if(nums[mid] == 0)
+            {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+
+        // O(2N) approach:-
+        /*
         int red = 0;
         int blue = 0;
         int white = 0;
@@ -30,5 +53,6 @@ public:
             nums[index] = 2;
             index++;
         }
+        */
     }
 };
